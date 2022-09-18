5cfb4a7c6e74bfae0aef8b9740100dbaf4f46eb4
Thomas Porter <thomas.porter.1991@gmail.com>
2022-09-18 14:03:46 -0400

    repaired slightly - still working

--- calendar.php
+++ calendar.php
@@ -1,28 +1,35 @@
+<?php
+//error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css" />
-<title>Field Trip Calendar</title>
-<script type="text/javascript">
-function returnDate(hour, mins, month, day, year)
-{
-	opener.resform.elements["arrival_hour"].value = hour;
-	opener.resform.elements["arrival_min"].value = mins;
-	opener.resform.elements["requested_month"].value = month;
-	opener.resform.elements["requested_day"].value = day;
-    opener.resform.elements["res_year"].value = year;
-	window.close()
-}
-</script>
-</head>
-<body class='form'>
-<?php 
-require_once 'config.php';
-$kid_count = $_GET['kid_count'];
-$kc = intval($kid_count);
-$date = time () ; 
-$day = date('d', $date);
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Field Trip Calendar</title>
+        <!-- javascript to get info from make res form -->
+        <script type="text/javascript">
+            function returnDate(hour, mins, month, day, year) {
+                opener.resform.elements["arrival_hour"].value = hour;
+                opener.resform.elements["arrival_min"].value = mins;
+                opener.resform.elements["requested_month"].value = month;
+                opener.resform.elements["requested_day"].value = day;
+                opener.resform.elements["res_year"].value = year;
+                window.close()
+            }
+        </script>
+    </head>
+    <body>
+    <?php 
+    require_once 'inc/config.php';
+    $kid_count = $_GET['kid_count'];
+    $kc = intval($kid_count);
+    $date = time () ; 
+    $day = date('d', $date);
 if (isset($_GET['m'])) {$month = $_GET['m'];} else {$month = date('m', $date); }
 if ($month<$spring_opening_month) {$month=$spring_opening_month;}
 if (isset($_GET['y'])) {$year = $_GET['y'];} else {$year = date('Y', $date);}  
@@ -44,20 +51,20 @@ $day_count = 1;
 <table border=1 cellpadding=0 cellspacing=0>
     <tr>
         <th colspan=7 class="header">
-            <? echo $title.' '.$year ?>
+            <?php echo $title.' '.$year ?>
         </th>
     </tr>
     <tr>
-        <th width=100><? link_prev_month($month, $year); ?></th>
+        <th width=100><?php link_prev_month($month, $year); ?></th>
         <th colspan=5>
-            <? echo $calendar_header_text; ?>
+            <?php echo $calendar_header_text; ?>
         </th>
-        <th width=100><? link_next_month($month, $year); ?></th>
+        <th width=100><?php link_next_month($month, $year); ?></th>
     </tr>
-    <tr><td align=center colspan=7><span class=greentext><? echo $greentext_text; ?></span></td></tr>
-    <tr><td align=center colspan=7><span class=orangetext><? echo $orangetext_text; ?></span></td></tr>
-    <tr><td align=center colspan=7><span class=error><? echo $redtext_text; ?></span></td></tr>
-    <tr><td align=center colspan=7><? echo $calendar_message; ?></td></tr>
+    <tr><td align=center colspan=7><span class=greentext><?php echo $greentext_text; ?></span></td></tr>
+    <tr><td align=center colspan=7><span class=orangetext><?php echo $orangetext_text; ?></span></td></tr>
+    <tr><td align=center colspan=7><span class=error><?php echo $redtext_text; ?></span></td></tr>
+    <tr><td align=center colspan=7><?php echo $calendar_message; ?></td></tr>
     <tr>
         <th width=100>Sunday</th>
         <th width=100>Monday</th>
