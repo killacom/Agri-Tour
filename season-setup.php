831b2ce30f268e3f49cacf217697dddf0d9e706f
Thomas Porter <thomas.porter.1991@gmail.com>
2022-09-18 18:57:00 -0400

    more season setup progress

--- season-setup.php
+++ season-setup.php
@@ -14,7 +14,7 @@ $thisfile = 'season-setup.php';
 $pagetitle = 'Season Settings';
 require_once 'inc/header.php';
 require_once 'inc/adminnav.php'; 
-$body = '';
+$body = $scripts = $onload = $onchange = $monthdata = '';
 
 //check seasons table
 $sql = 'SELECT * FROM `seasons`';
@@ -27,16 +27,69 @@ if (isset($_POST['add_seasons'])) {
     $num_seasons_to_add  = $_POST['num_seasons_to_add'];
     //display form to add seasons
     $body.= '
-        <form action='.$thisfile.' method="POST">
+        <form action='.$thisfile.' method="POST" name="add_seasons_form" id="add_seasons_form">
             <table>
+            <tr>
+                <td>Season #</td><td>Season Name</td><td>Opening Date</td><td>Opening Time</td> 
+            </tr>
     ';
-   for ($a = 0; $a < $num_seasons_to_add; $a++) {
+   for ($a = 1; $a < $num_seasons_to_add+1; $a++) {
     $body.= '
+                
                 <tr>
-                    <td>Season '.($a+1).'</td><td><input type="text" name="season'.$a.'name"></td>
+                    <td>Season '.$a.'</td>
+                    <td><input type="text" name="season'.$a.'name"></td>
+                    <td>
+                        <select name="season_'.$a.'_open_month" id="season_'.$a.'_open_month">';
+                        for ($rm = 1; $rm < 13; $rm++) {
+                            $body.='<option value="'.$rm.'">'.getmonthname($rm).'</option>';
+                        }
+                $body.= '</select>
+                        <select name="season_'.$a.'_open_day" id="season_'.$a.'_open_day">
+                        </select>
                 </tr>
     ';
    }
+    $scripts = '
+        <script type="text/javascript">
+    ';
+    for ($i = 0; $i < $num_seasons_to_add; $i++) {
+        $j=$i+1;
+        
+        $onchange.= "
+            document.forms['add_seasons_form'].elements['season_".$j."_open_month'].onchange = function(e) {
+                var relName = 'season_".$j."_open_day';
+                var relList = this.form.elements[ relName ];
+                var obj = Select_List_Data".$j."[ relName ][ this.value ];
+                removeAllOptions(relList, true);
+                appendDataToSelect(relList, obj);
+            };";
+        $monthdata.= "
+            var Select_List_Data".$j." = {
+                'season_".$j."_open_day': {
+                    ".$month_switcher."
+                }
+            };
+        ";
+        $onload.= "
+                var sel".$j." = form.elements['season_".$j."_open_month'];
+                var relName".$j." = 'season_".$j."_open_day';
+                var rel".$j." = form.elements[ relName".$j." ];
+                var data".$j." = Select_List_Data".$j."[ relName".$j." ][ sel".$j.".value ];
+                appendDataToSelect(rel".$j.", data".$j.");
+        ";
+    }
+    $scripts.= $onchange.$monthdata."            
+            window.onload = function() {
+                var form = document.forms['add_seasons_form'];
+                ".$onload." 
+            };
+
+        </script>
+    ";
+
+
+
     $body.= '
         </table>
    </form><br><br>';
@@ -48,16 +101,16 @@ if ($rowcount == 0) {
 } else {
 //or print seasons
 $body =  '
-    <form action="'.$thisfile.'" method="POST">
+    <form action="'.$thisfile.'" method="POST" name="add_seasons_form">
         <table>';
-        for ($s = 0; ($s < $num_seasons); $s++) {
+        for ($s = 1; ($s < $num_seasons); $s++) {
 $body.=  '
             <tr>
                 <td>
                     Opening Date: 
                 </td>
                 <td> 
-                    <select name=spring_opening_month_new>';
+                    <select name="season_'.$s.'_opening_month">';
                         for ($i = 1; $i < 12; $i++) {
                             $body.= '<option value='.$i;
                             if ($spring_opening_month==$i) {
@@ -66,12 +119,13 @@ $body.=  '
                             $body.= '>'.getmonthname($i).'</option>';
                         }
          $body.=   '</select>
-                    <input type="text" name="spring_opening_day_new" value="'.$opening_day.'" size="2">
+                    <select name="season_'.$s.'_opening_month" id="season_'.$s.'_opening_month">
+                    </select>
                 </td>
             </tr>
             <tr>
                 <td>
-                    Spring Closing Date: 
+                    Closing Date: 
                 </td>
                 <td> 
                     <select name="spring_closing_month_new">';
@@ -116,4 +170,7 @@ $body.=  '
     echo $body;
     require_once 'inc/footer.php';
 
+echo $scripts;
+
 ?>
+
