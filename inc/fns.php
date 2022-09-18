23f6cfbf0b22a8984ecbc1dbe3a7cfb71556fc46
Thomas Porter <thomas.porter.1991@gmail.com>
2022-09-18 14:26:59 -0400

    cleaned up code some and added some comments

--- inc/fns.php
+++ inc/fns.php
@@ -1,13 +1,14 @@
 <?php
 
-function daycount($m)
-{
+//get how many days are in month $m (int)
+function daycount($m) {
     if ($m==1 || $m==3 || $m==5 || $m==7 || $m==8 || $m==10 || $m==12){return 31;}
     elseif ($m==4 || $m==6 || $m==9 || $m==11){return 30;}
     elseif ($m==2){return 28;}
 }
-function getmonthname($m)
-{
+
+//get month name by $m (int)
+function getmonthname($m) {
     $name ='';
     switch (intval($m)) 
     {
@@ -26,114 +27,123 @@ function getmonthname($m)
     }
     return $name;
 }
-function validEmail($email)
-{
+
+//check input $email to make sure right format for an email address
+function validEmail($email){
    $isValid = true;
    $atIndex = strrpos($email, "@");
-   if (is_bool($atIndex) && !$atIndex)
-   {
+   if (is_bool($atIndex) && !$atIndex) {
       $isValid = false;
-   }
-   else
-   {
+   } else {
       $domain = substr($email, $atIndex+1);
       $local = substr($email, 0, $atIndex);
       $localLen = strlen($local);
       $domainLen = strlen($domain);
-      if ($localLen < 1 || $localLen > 64)
-      {
+      if ($localLen < 1 || $localLen > 64) {
          $isValid = false;
-      }
-      else if ($domainLen < 1 || $domainLen > 255)
-      {
+      } else if ($domainLen < 1 || $domainLen > 255) {
          $isValid = false;
-      }
-      else if ($local[0] == '.' || $local[$localLen-1] == '.')
-      {
+      } else if ($local[0] == '.' || $local[$localLen-1] == '.') {
          $isValid = false;
-      }
-      else if (preg_match('/\\.\\./', $local))
-      {
+      } else if (preg_match('/\\.\\./', $local)) {
          $isValid = false;
-      }
-      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
-      {
+      } else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
          $isValid = false;
-      }
-      else if (preg_match('/\\.\\./', $domain))
-      {
+      } else if (preg_match('/\\.\\./', $domain)) {
          $isValid = false;
-      }
-      else if
-(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
-                 str_replace("\\\\","",$local)))
-      {
-         if (!preg_match('/^"(\\\\"|[^"])+"$/',
-             str_replace("\\\\","",$local)))
-         {
+      } else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/', str_replace("\\\\","",$local))) {
+         if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\","",$local))) {
             $isValid = false;
          }
       }
-      if ($isValid && !(checkdnsrr($domain,"MX") || 
- checkdnsrr($domain,"A")))
-      {
+      if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A"))) {
          $isValid = false;
       }
    }
    return $isValid;
 }
-function link_next_month($m, $y) 
-{
+
+//link to next month for calender page
+function link_next_month($m, $y) {
     global $som, $scm, $fom, $fcm, $kc;
     $next_month = intval($m)+1;
     $season = season_num($next_month);
-    if($season == 2) {$next_month=$fom;}
-	if ($next_month==13) {$next_month=1; $year=intval($y)+1;} else {$year=$y;}
-    $date =time(); 
+    if($season == 2) {
+        $next_month = $fom;
+    }
+	if ($next_month==13) {
+        $next_month = 1; 
+        $year = intval($y)+1;
+    } else {
+        $year = $y;
+    }
+    $date = time(); 
 	$cur_year = date('Y', $date);
-    $href = '<a href="calendar.php?m='.$next_month.'&y='.$year.'&kid_count='.$kc.'"><span class=twelve>Next Month</span><br /><img src="images/right.gif" width=60 height=40></a>';
-	//if ($year!=intval($cur_year) || $next_month > $fcm)  { echo '&nbsp;'; } else { echo $href; }     
+    $href = '<a href="calendar.php?m='.$next_month.'&y='.$year.'&kid_count='.$kc.'"><span class=twelve>Next Month</span><br /><img src="images/right.gif" width=60 height=40></a>';    
     echo $href;
 }
+
+//link to prev month for calender page
 function link_prev_month($m, $y) 
 {
     global $som, $scm, $fom, $fcm, $kc;
 	$last_month = intval($m)-1;
     $season = season_num($last_month);
-    if($season == 2) {$last_month=$scm;}
-	if ($last_month==0) {$last_month=12; $year=intval($y)-1;} else {$year=$y;}
-	$date =time(); 
+    if($season == 2) {
+        $last_month=$scm;
+    }
+	if ($last_month==0) {
+        $last_month = 12; 
+        $year = intval($y)-1;
+    } else {
+        $year = $y;
+    }
+	$date = time(); 
 	$cur_year = date('Y', $date);
     $href = '<a href="calendar.php?m='.$last_month.'&y='.$year.'&kid_count='.$kc.'"><span class=twelve>Prev. Month</span><br /><img src="images/left.gif" width=60 height=40></a>';
-	//if ($year!=intval($cur_year) || $last_month < $som) {echo '&nbsp;';} else {echo $href;}
     echo $href;
 }
-function link_next_month_school($m, $y) 
-{
+
+//link to next month for school display calender page
+function link_next_month_school($m, $y) {
     global $som, $scm, $fom, $fcm;
 	$next_month = intval($m)+1;
     $season = season_num($next_month);
-    if($season == 2) {$next_month=$fom;} 
-	if ($next_month==13) {$next_month=1; $year=intval($y)+1;} else {$year=$y;}
-    $date =time(); 
+    if($season == 2) {
+        $next_month = $fom;
+    } 
+	if ($next_month==13) {
+        $next_month = 1; 
+        $year = intval($y)+1;
+    } else {
+        $year=$y;
+    }
+    $date = time(); 
 	$cur_year = date('Y', $date);
     $href = '<a href="'.$_SERVER['PHP_SELF'].'?m='.$next_month.'&y='.$year.'"><span class=twelve>Next Month</span><br /><img src="images/right.gif" width=60 height=40></a>';
-	//if ($year!=intval($cur_year) || $next_month > $fcm)  { echo "&nbsp;"; }  else { echo $href; }
     echo $href;
 }
-function link_prev_month_school($m, $y) 
-{
+
+//link to prev month for school display calender page
+function link_prev_month_school($m, $y) {
     global $som, $scm, $fom, $fcm;
 	$last_month = intval($m)-1;
     $season = season_num($last_month);
-    if($season == 2) {$last_month=$scm;}
-	if ($last_month==0) {$last_month=12; $year=intval($y)-1;} else {$year=$y;}
-	$date =time(); 
+    if($season == 2) {
+        $last_month = $scm;
+    }
+	if ($last_month==0) {
+        $last_month = 12; 
+        $year = intval($y)-1;
+    } else {
+        $year = $y;
+    }
+	$date = time(); 
 	$cur_year = date('Y', $date);
     $href = '<a href="'.$_SERVER['PHP_SELF'].'?m='.$last_month.'&y='.$year.'"><span class=twelve>Prev. Month</span><br /><img src="images/left.gif" width=60 height=40></a>';
-	//if ($year!=intval($cur_year) || $last_month < $som) {echo "&nbsp;";} else echo $href;
     echo $href;
 }
+
 function print_cal_dates($cl, $h, $m, $d, $y)
 {
     echo "
